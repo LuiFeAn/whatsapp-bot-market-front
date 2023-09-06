ef6c87810b04b2811a2f3ea538cce15de800a497
Luis <luisfernandogvv@gmail.com>
2023-09-06 18:55:12 -0300

    feat: at

--- src/pages/Clients/index.jsx
+++ src/pages/Clients/index.jsx
@@ -17,6 +17,7 @@ import { Input } from '../../components/Input';
 import Button from '../../components/Button';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
+import ClientForm from './Form';
 
 export default function Clients(){
 
@@ -44,39 +45,17 @@ export default function Clients(){
 
     const [ forEdit, setForEdit ] = useState(false);
 
+    const [ forInsert, setForInsert ] = useState(false);
+
     useEffect( () => {
 
         if( registerNewUserModal ){
 
-            setForEdit(false);
+           setForInsert(true);
 
         }
 
-    },[]);
-
-    const registerClientSchema = yup.object().shape({
-        whatsapp: yup.string().required('Informe o número do Whatsapp'),
-        contactNumber: yup.string().required('Informe o número para contato'),
-        fullName: yup.string().required('Informe o nome completo').max(150),
-        adress: yup.string().notRequired().max(150),
-        neighborhood: yup.string().notRequired(),
-        houseNumber: yup.string().notRequired(),
-        complement: yup.string().notRequired()
-    });
-
-    const registerClientValidation = useFormik({
-        initialValues:{
-            whatsapp:'',
-            contactNumber:'',
-            fullName:'',
-            adress:'',
-            neighborhood:'',
-            houseNumber:'',
-            complement:'',
-        },
-        validationSchema: registerClientSchema,
-        onSubmit:registerNewClient
-    });
+    },[registerNewUserModal]);
 
     async function getUsers(){
 
@@ -194,9 +173,13 @@ export default function Clients(){
         setRegisterNewUserModal(true);
     }
 
-    async function registerNewClient(event){
+    async function registerNewClient(infos){
 
-       console.log('ok')
+        const promise = econoAPI.post('/users',infos);
+
+        await toast.promise(promise,{
+            success:'Cliente cadastrado com sucesso',
+        })
 
     }
 
@@ -226,25 +209,9 @@ export default function Clients(){
 
             <Button style={ { height:'40px',border:'none'}} onClick={newRegister}>CADASTRAR NOVO CLIENTE</Button>
 
-            <Modal visible={registerNewUserModal} confirmHandler={registerClientValidation.handleSubmit} titleColor={'blue'} cancelHandler={cancelRegister} confirmTitle='Cadastrar' CancelTitle='Cancelar' title={'CADASTRAR NOVO USUÁRIO'}>
+            <Modal visible={registerNewUserModal} confirmHandler={registerNewClient} titleColor={'blue'} cancelHandler={cancelRegister} confirmTitle='Cadastrar' CancelTitle='Cancelar' title={'CADASTRAR NOVO USUÁRIO'}>
 
-                <S.NewRegisterContainer>
-
-                    <Input {...registerClientValidation.getFieldProps('whatsapp')} mask='(99) 99999-9999' placeholder='N° Whatsapp'></Input>
-
-                    <Input {...registerClientValidation.getFieldProps('contactNumber')} mask='(99) 99999-9999' placeholder='N° Telefone para Contato'></Input>
-
-                    <Input {...registerClientValidation.getFieldProps('fullName')} placeholder='Nome Completo'></Input>
-
-                    <Input {...registerClientValidation.getFieldProps('adress')} placeholder='Endereço'></Input>
-
-                    <Input {...registerClientValidation.getFieldProps('neighborhood')} placeholder='Bairro'></Input>
-
-                    <Input {...registerClientValidation.getFieldProps('houseNumber')} placeholder='N° Casa'></Input>
-
-                    <Input {...registerClientValidation.getFieldProps('complement')} placeholder='Complemento'></Input>;
-
-                </S.NewRegisterContainer>
+                <ClientForm onSubmit={registerNewClient} forEdit={false}/>
 
             </Modal>
 
