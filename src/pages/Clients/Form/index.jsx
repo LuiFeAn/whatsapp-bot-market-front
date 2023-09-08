886b9216a5101eae3177dae2e4940c87bc67a3e6
Luis <luisfernandogvv@gmail.com>
2023-09-08 17:08:46 -0300

    feat: att

--- src/pages/Clients/Form/index.jsx
+++ src/pages/Clients/Form/index.jsx
@@ -13,18 +13,27 @@ import Modal from '../../../components/Modal';
 import phoneNumberMask from '../../../utils/phoneNumberMask';
 
 import convertToPhoneId from '../../../utils/convertToPhoneId';
+
 import { useEffect } from 'react';
 
 export default function ClientForm({ onSubmit, forEdit, visible, onCancelSubmit, title, clearFields }){
 
     const registerClientSchema = yup.object().shape({
-        whatsapp: yup.string()[ !forEdit ? 'required' : 'notRequired']('Informe o número do Whatsapp').min(11,'Número de Whatsapp inválido'),
-        contactNumber: yup.string().notRequired('Informe o número para contato').min(11,'Número para contato inválido'),
-        fullName: yup.string()[ !forEdit ? 'required' : 'notRequired']('Informe o nome completo').max(150,'O nome deve possuir no máximo 150 caracteres'),
-        adress: yup.string().notRequired().max(100,'O endereço deve possuir no máximo 150 caracteres'),
-        neighborhood: yup.string().notRequired().max(65,'O bairro deve possuir no máximo 65 caracteres'),
-        houseNumber: yup.string().notRequired().max(100000,'O Número da casa deve possuir no máximo 100000 caracteres'),
-        complement: yup.string().notRequired().max(100,'O complemento deve possuir no máximo 100 caracteres')
+        whatsapp: yup.
+        string()[ !forEdit ? 'required' : 'notRequired']('Informe o número do Whatsapp').
+        min(11,'Número de Whatsapp inválido'),
+        contactNumber: yup.string().notRequired('Informe o número para contato').
+        min(11,'Número para contato inválido'),
+        fullName: yup.string()[ !forEdit ? 'required' : 'notRequired']('Informe o nome completo').
+        max(150,'O nome deve possuir no máximo 150 caracteres'),
+        adress: yup.string().notRequired().
+        max(100,'O endereço deve possuir no máximo 150 caracteres'),
+        neighborhood: yup.string().notRequired().
+        max(65,'O bairro deve possuir no máximo 65 caracteres'),
+        houseNumber: yup.string().notRequired().
+        max(100000,'O Número da casa deve possuir no máximo 100000 caracteres'),
+        complement: yup.string().notRequired().
+        max(100,'O complemento deve possuir no máximo 100 caracteres')
     });
 
     const registerClientValidation = useFormik({
@@ -41,8 +50,8 @@ export default function ClientForm({ onSubmit, forEdit, visible, onCancelSubmit,
         validationSchema: registerClientSchema,
         onSubmit: async values => await onSubmit({
             ...values,
-            forEdit,
-            whatsapp: values.whatsapp.length > 0 ? convertToPhoneId(values.whatsapp) : ''
+            contactNumber: convertToPhoneId(values.contactNumber).normal,
+            whatsapp: values.whatsapp.length > 0 ? convertToPhoneId(values.whatsapp).withId : ''
         })
     });
 
--- src/utils/convertToPhoneId.js
+++ src/utils/convertToPhoneId.js
@@ -4,6 +4,9 @@ export default function convertToPhoneId(number){
 
     number = number.replace(/\D/g, '');
 
-    return `55${number}@c.us`
+    return {
+        withId: `55${number}@c.us`,
+        normal: number
+    }
 
 }
